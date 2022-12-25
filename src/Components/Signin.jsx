159cfba40a8f1fd7e921f1d89ecb1ba833af9e92
kafi-anan-rafi <kafianan01@gmail.com>
2022-12-25 20:57:57 +0600

    Signin option improved

--- src/Components/Signin.jsx
+++ src/Components/Signin.jsx
@@ -1,39 +1,38 @@
 import Axios from 'axios'
-import React, { useEffect, useState } from 'react'
+import React, {  useState } from 'react'
 import { useNavigate } from 'react-router-dom';
 import config from '../config.json';
 
 function Signin() {
-  const [users, setUsers] = useState([]);
-  const navigate = useNavigate();
-  useEffect(() => {
-    Axios.get(config.apiURL + "posts")
-      .then(res => {
-        console.log(res)
-        setUsers(res.data);
-      })
-      .catch(err => {
-        console.log(err);
-      })
-  }, []);
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [alert, setAlert] = useState(false);
+  
+  // const navigate = useNavigate();
 
   const handleSubmit = (event) => {
     event.preventDefault();
-    const email = event.target.email.value;
-    const password = event.target.password.value;
+    Axios.post(config.apiURL + "login", {
+      Email: email,
+      Password: password
+    })
+      .then(res => {
+        console.log(res)
+      })
+      .catch(err => {
+        setAlert(true)
+        console.log(err);
+      })
+  }
+  const handleEmailChange = (e) => {
+    setEmail(e.target.value);
+    console.log(e.target.value);
+  }
 
-    if (email && password) {
-      if (users.type === "farmer")
-        navigate("/farmer")
-      if (users.type === "customer")
-        navigate("/customer")
-      if (users.type === "doctor")
-        navigate("/doctor")
-      if (users.type === "advisor")
-        navigate("/advisor")
-    } else {
-      console.log("No");
-    }
+  const handlePasswordChange = (e) => {
+    setPassword(e.target.value);
+    console.log(e.target.value);
+
   }
 
   return (
@@ -41,12 +40,13 @@ function Signin() {
       <form onSubmit={handleSubmit} className="col-5">
         <div className="form-group">
           <label className='mt-20' htmlFor="email">Email address</label>
-          <input type="email" className="form-control" id="email" name='email' />
+          <input type="email" onChange={handleEmailChange} className="form-control" id="email" name='email' />
         </div>
         <div className="form-group">
           <label htmlFor="password">Password</label>
-          <input type="password" className="form-control" id="password" name='password' />
+          <input type="password" onChange={handlePasswordChange} className="form-control" id="password" name='password' />
         </div>
+        {alert && <div className='alert alert-danger'>Email or Password incorrect</div>}
         <button type="submit" className="btn btn-primary">Login</button>
       </form>
     </div>
